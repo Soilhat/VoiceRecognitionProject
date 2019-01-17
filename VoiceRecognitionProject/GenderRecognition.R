274b464a22ac1e7429ef5e4ab5232851ee8877a2
Soilhat <soilhatmohamed@gmail.com>
2019-01-17 17:46:18 +0100

    Add test open Wave file

--- VoiceRecognitionProject/GenderRecognition.R
+++ VoiceRecognitionProject/GenderRecognition.R
@@ -57,3 +57,30 @@ findgender <- function(voicedata) # Using Logistic Regression
 dataset[1586,1:20]
 test = findgender(dataset[1586,1:20])
 test
+
+library("tuneR")
+library("audio")
+library("warbleR")
+data(list = c("file", "selec.table"))
+writeWave(file,"file.wav")
+
+wav <- readMP3("test.mp3")
+sp <- specan(selec.table[1,], pb=FALSE)
+
+truc <- data(wav)
+
+# First set temporary folder
+
+data(list = c("Phae.long1", "Phae.long2", "Phae.long3", "selec.table"))
+writeWave(Phae.long1,"Phae.long1.wav")
+writeWave(Phae.long2,"Phae.long2.wav")
+writeWave(Phae.long3,"Phae.long3.wav")
+
+# measure acoustic parameters
+sp_param <- specan(X = selec.table[1:8,], pb = FALSE)
+
+# measuring peakf
+sp_param <- specan(X = selec.table[1:8,], pb = FALSE, fast = FALSE)
+
+# measuring harmonic-related parameters using progress bar
+sp_param <- specan(X = selec.table[1:8,], harmonicity = TRUE)
